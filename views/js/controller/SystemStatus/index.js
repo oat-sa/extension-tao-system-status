1938322826640fde11fe8031ddff37d607edca8e
Anton Tsymuk <atsymuk@gmail.com>
2019-12-26 10:11:26 +0300

    refactoring

--- views/js/controller/SystemStatus/index.js
+++ views/js/controller/SystemStatus/index.js
@@ -6,154 +6,178 @@ define([
     'tpl!taoSystemStatus/controller/SystemStatus/tpl/chartsContainer',
     'tpl!taoSystemStatus/controller/SystemStatus/tpl/taskQueue',
     'taoSystemStatus/tasksMonitoring/tasksStatistics',
-    'c3'
-], function ($, __, reportsDataProvider, reportTableTpl, chartsContainerTpl, taskQueueTpl, tasksStatistics, c3) {
-    'use strict';
+    'layout/loading-bar',
+    'ui/feedback',
+    'c3',
+], function (
+    $,
+    __,
+    reportsDataProvider,
+    reportTableTpl,
+    chartsContainerTpl,
+    taskQueueTpl,
+    tasksStatistics,
+    loadingBar,
+    feedback,
+    c3
+) {
+        'use strict';
 
-    const reportCategories = {
-        configuration: 'TAO Configuration',
-        configurationValues: 'Configuration Values',
-        healthReadinessCheck: 'Health/Readiness check',
-        monitoringStatistics: 'Monitoring / Statistics',
-    }
-
-    return {
-        start: function () {
-            const $container = $('#system-status-report');
-            const $configurationTablesContainer = $('#system-status-configuration-tables');
-            const {
-                configuration,
-                configurationValues,
-                healthReadinessCheck,
-                monitoringStatistics,
-            } = reportCategories;
-
-            const renderMonitoringStatistics = {
-                'oat\\taoSystemStatus\\model\\Check\\System\\TaskQueueFailsCheck': ({ children }) => {
-                    $container.append($(reportTableTpl({
-                        category: configuration,
-                        columns: [__('Task'), __('Date'), ''],
-                        data: children
-                            .map(({ message }) => ({
-                                rows: [message, new Date().toLocaleString()],
-                                detailsButton: true,
-                            }))
-                    })));
-                },
-                'oat\\taoSystemStatus\\model\\Check\\System\\TaskQueueFinishedCheck': ({ data: { P1D, P1W, P1M } }) => {
-                    const $graphContainer = $('<div></div>');
-                    $container.append($graphContainer);
-
-                    tasksStatistics({
-                        defaultInterval: 'P1D',
-                        intervals: [
-                            { label: __('Last Day'), value: 'P1D' },
-                            { label: __('Last Week'), value: 'P1W' },
-                            { label: __('Last Month'), value: 'P1M' }
-                        ],
-                        data: { P1D, P1W, P1M },
-                    })
-                        .render($graphContainer);
-                }
-            };
-
-            reportsDataProvider.getReports().then(({ report: { children: reports } }) => {
-                // Group reports by category
-                const reportGroups = reports.reduce(
-                    (agg, item) => {
-                        const { data: { category } } = item;
-
-                        agg[category] && agg[category].push(item);
-
-                        return agg;
-                    },
-                    {
-                        [configuration]: [],
-                        [configurationValues]: [],
-                        [healthReadinessCheck]: [],
-                        [monitoringStatistics]: [],
-                    }
-                );
-
-                const $configurationTable = $(reportTableTpl({
-                    category: configuration,
-                    columns: [__('Status'), __('Description'), __('Date')],
-                    data: reportGroups[configuration]
-                        .map(({ type, data: { details, date } }) => ({
-                            type,
-                            [`is${type}`]: true,
-                            rows: [details, new Date(date * 1000).toLocaleString()],
-                        }))
-                }));
-
-                const $configurationValuesTable = $(reportTableTpl({
-                    category: configurationValues,
-                    columns: [__('Status'), __('Description'), __('Date')],
-                    data: reportGroups[configurationValues]
-                        .map(({ type, message, data: { details } }) => ({
-                            type,
-                            [`is${type}`]: true,
-                            rows: [details, message],
-                        }))
-                }));
-
-                const $healthReadinessStatusTable = $(reportTableTpl({
-                    category: healthReadinessCheck,
-                    columns: [__('Status'), __('Description'), __('Date')],
-                    data: reportGroups[healthReadinessCheck]
-                        .map(({ type, message, data: { details } }) => ({
-                            type,
-                            [`is${type}`]: true,
-                            rows: [details, message],
-                        }))
-                }));
-
-                $configurationTablesContainer.append($configurationTable);
-                $configurationTablesContainer.append($configurationValuesTable);
-                $container.append($healthReadinessStatusTable);
-
-                reportGroups[monitoringStatistics]
-                    .forEach((check) => {
-                        const { data: { check_id } } = check;
-
-                        renderMonitoringStatistics[check_id] && renderMonitoringStatistics[check_id](check);
-                    });
-
-                const $chartContainer = $(chartsContainerTpl());
-                $container.append($chartContainer);
-
-                c3.generate({
-                    bindto: '.js-tasks-donut-2',
-                    data: {
-                        columns: [
-                            ['data1', 30],
-                            ['data2', 120],
-                        ],
-                        type: 'donut',
-                    },
-                    donut: {
-                        title: 'Iris Petal Width',
-                    },
-                    width: 500,
-                });
-
-                c3.generate({
-                    bindto: '.js-tasks-donut-1',
-                    data: {
-                        columns: [
-                            ['data1', 30],
-                            ['data2', 120],
-                        ],
-                        type: 'donut',
-                    },
-                    donut: {
-                        title: 'Iris Petal Width'
-                    }
-                });
-
-                const $taskQueue = $(taskQueueTpl());
-                $chartContainer.append($taskQueue);
-            })
+        const reportCategories = {
+            configuration: 'TAO Configuration',
+            configurationValues: 'Configuration Values',
+            healthReadinessCheck: 'Health/Readiness check',
+            monitoringStatistics: 'Monitoring / Statistics',
         }
-    }
-});
+
+        return {
+            start: function () {
+                const $container = $('#system-status-report');
+                const $configurationTablesContainer = $('#system-status-configuration-tables');
+                const {
+                    configuration,
+                    configurationValues,
+                    healthReadinessCheck,
+                    monitoringStatistics,
+                } = reportCategories;
+
+                const renderMonitoringStatistics = {
+                    'oat\\taoSystemStatus\\model\\Check\\System\\TaskQueueFailsCheck': ({ children = [], data: { details } }) => {
+                        if (!children.length) {
+                            return;
+                        }
+
+                        $container.append($(reportTableTpl({
+                            category: details,
+                            columns: [__('Task'), __('Date'), ''],
+                            data: children
+                                .map(({ message }) => ({
+                                    rows: [message, new Date().toLocaleString()],
+                                    detailsButton: true,
+                                }))
+                        })));
+                    },
+                    'oat\\taoSystemStatus\\model\\Check\\System\\TaskQueueFinishedCheck': ({ data: { P1D = [], P1W = [], P1M = [] } }) => {
+                        const $graphContainer = $('<div></div>');
+                        $container.append($graphContainer);
+
+                        tasksStatistics({
+                            defaultInterval: 'P1D',
+                            intervals: [
+                                { label: __('Last Day'), value: 'P1D' },
+                                { label: __('Last Week'), value: 'P1W' },
+                                { label: __('Last Month'), value: 'P1M' }
+                            ],
+                            data: { P1D, P1W, P1M },
+                        })
+                            .render($graphContainer);
+                    }
+                };
+
+                loadingBar.start();
+
+                reportsDataProvider.getReports()
+                    .then(({ report: { children: reports } }) => {
+                        // Group reports by category
+                        const reportGroups = reports.reduce(
+                            (agg, item) => {
+                                const { data: { category } } = item;
+
+                                agg[category] && agg[category].push(item);
+
+                                return agg;
+                            },
+                            {
+                                [configuration]: [],
+                                [configurationValues]: [],
+                                [healthReadinessCheck]: [],
+                                [monitoringStatistics]: [],
+                            }
+                        );
+
+                        if (reportGroups[configuration].length) {
+                            $configurationTablesContainer.append($(reportTableTpl({
+                                category: configuration,
+                                columns: [__('Status'), __('Description'), __('Date')],
+                                data: reportGroups[configuration]
+                                    .map(({ type, data: { details, date } }) => ({
+                                        type,
+                                        [`is${type}`]: true,
+                                        rows: [details, new Date(date * 1000).toLocaleString()],
+                                    }))
+                            })));
+                        }
+
+                        if (reportGroups[configurationValues].length) {
+                            $configurationTablesContainer.append($(reportTableTpl({
+                                category: configurationValues,
+                                columns: [__('Status'), __('Description'), __('Value')],
+                                data: reportGroups[configurationValues]
+                                    .map(({ type, message, data: { details } }) => ({
+                                        type,
+                                        [`is${type}`]: true,
+                                        rows: [details, message],
+                                    }))
+                            })));
+                        }
+
+                        if (reportGroups[healthReadinessCheck].length) {
+                            $container.append($(reportTableTpl({
+                                category: healthReadinessCheck,
+                                columns: [__('Status'), __('Description'), __('Details')],
+                                data: reportGroups[healthReadinessCheck]
+                                    .map(({ type, message, data: { details } }) => ({
+                                        type,
+                                        [`is${type}`]: true,
+                                        rows: [details, message],
+                                    }))
+                            })));
+                        }
+
+                        reportGroups[monitoringStatistics]
+                            .forEach((check) => {
+                                const { data: { check_id } } = check;
+
+                                renderMonitoringStatistics[check_id] && renderMonitoringStatistics[check_id](check);
+                            });
+
+                        const $chartContainer = $(chartsContainerTpl());
+                        $container.append($chartContainer);
+
+                        c3.generate({
+                            bindto: '.js-tasks-donut-2',
+                            data: {
+                                columns: [
+                                    ['data1', 30],
+                                    ['data2', 120],
+                                ],
+                                type: 'donut',
+                            },
+                            donut: {
+                                title: 'Iris Petal Width',
+                            },
+                            width: 500,
+                        });
+
+                        c3.generate({
+                            bindto: '.js-tasks-donut-1',
+                            data: {
+                                columns: [
+                                    ['data1', 30],
+                                    ['data2', 120],
+                                ],
+                                type: 'donut',
+                            },
+                            donut: {
+                                title: 'Iris Petal Width'
+                            }
+                        });
+
+                        const $taskQueue = $(taskQueueTpl());
+                        $chartContainer.append($taskQueue);
+                    })
+                    .catch(() => feedback().error(__('Something went wrong.')))
+                    .loadingBar(() => loadingBar.stop());
+            }
+        }
+    });
--- views/templates/Status/index.tpl
+++ views/templates/Status/index.tpl
@@ -5,6 +5,7 @@ use oat\tao\helpers\Template;
 <link rel="stylesheet" href="<?= Template::css('systemstatus.css') ?>" />
 
 <div class="status-report-container" id="system-status-report">
-  <div class="status-report-configuration-tables" id="system-status-configuration-tables">
-  </div>
+    <h1 class="status-report-header"><?= __("Welcome to TAO's status page hub") ?></h1>
+    <div class="status-report-configuration-tables" id="system-status-configuration-tables">
+    </div>
 </div>
