8efbb99f10ef5274feced5ca759815203f9b5c58
Aleksej Tikhanovich <aleksej@taotesting.com>
2020-02-28 13:04:42 +0300

    Fixed bundle

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -31,6 +31,7 @@ module.exports = function(grunt) {
                     outputDir : 'loader',
                     bundles : [{
                         name : 'taoSystemStatus',
+                        babel: true,
                         default : true,
                     }]
                 }
