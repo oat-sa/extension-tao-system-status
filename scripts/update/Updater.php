3172472c3fd38bfd3c67d22f1dfd34ce9a998217
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-03-31 09:48:22 +0300

    syncModels

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -65,6 +65,11 @@ class Updater extends common_ext_ExtensionUpdater
 
             $this->setVersion('0.11.0');
         }
+
+        if ($this->isVersion('0.11.0')) {
+            OntologyUpdater::syncModels();
+            $this->setVersion('0.11.1');
+        }
     }
 }
 
