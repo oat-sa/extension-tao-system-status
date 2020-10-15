d95e590de122bdb24dd7f247d61a8b2ecbe392d6
Aleh Hutnikau <hutnikau@1pt.com>
2020-10-15 11:31:40 +0300

    use strict types

--- model/Check/System/FileSystemS3CachePathCheck.php
+++ model/Check/System/FileSystemS3CachePathCheck.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -14,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2019 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
