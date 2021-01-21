00219005aba0c0656fdc47992cf53d29836172f6
Aleh Hutnikau <hutnikau@1pt.com>
2021-01-21 15:34:29 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return [
     'label' => 'TAO System Status',
     'description' => 'TAO System Status',
     'license' => 'GPL-2.0',
-    'version' => '0.17.0',
+    'version' => '0.18.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'tao' => '>=46.11.1',
