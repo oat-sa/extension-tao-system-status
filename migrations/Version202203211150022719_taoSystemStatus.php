c3503e16e64e4625d92ab7912dccbe5f00d5fe5b
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2022-03-21 17:33:49 +0300

    chore: using container load of service

--- migrations/Version202203211150022719_taoSystemStatus.php
+++ migrations/Version202203211150022719_taoSystemStatus.php
@@ -9,9 +9,6 @@ use oat\tao\scripts\tools\migrations\AbstractMigration;
 use oat\taoSystemStatus\model\Check\System\CertificateCheck;
 use oat\taoSystemStatus\model\SystemStatus\SystemStatusService;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version202203211150022719_taoSystemStatus extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,19 +16,24 @@ final class Version202203211150022719_taoSystemStatus extends AbstractMigration
         return sprintf('Remove check `%s` from the check list', CertificateCheck::class);
     }
 
+    /**
+     * @throws \oat\taoSystemStatus\model\SystemStatusException
+     */
     public function up(Schema $schema): void
     {
-        $check = new CertificateCheck([]);
-        /** @var SystemStatusService $systemStatusService */
-        $systemStatusService = $this->getServiceLocator()->get(SystemStatusService::SERVICE_ID);
-        $systemStatusService->removeCheck($check);
+        $this->getSystemStatusService()->removeCheck(new CertificateCheck([]));
     }
 
+    /**
+     * @throws \oat\taoSystemStatus\model\SystemStatusException
+     */
     public function down(Schema $schema): void
     {
-        $check = new CertificateCheck([]);
-        /** @var SystemStatusService $systemStatusService */
-        $systemStatusService = $this->getServiceLocator()->get(SystemStatusService::SERVICE_ID);
-        $systemStatusService->addCheck($check);
+        $this->getSystemStatusService()->addCheck(new CertificateCheck([]));
+    }
+
+    private function getSystemStatusService(): SystemStatusService
+    {
+        return $this->getServiceLocator()->getContainer()->get(SystemStatusService::SERVICE_ID);
     }
 }
